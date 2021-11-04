c7410f4a03d24ce201b242b3c2f38ee3e3bf8bf6
Danielr.looken <Danielr.looken@gmail.com>
2021-11-04 14:25:41 +0100

    change the allocate() function so it will detect an error from malloc(). When an error is detected, the program should print a text describing the error with perror() and exit the program.

--- Assignment_1/Assignment_C/allocate.c
+++ Assignment_1/Assignment_C/allocate.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 void allocate(int value)
 {
     int *ptr = NULL;
     ptr = malloc(100000 * sizeof(int));
+     if (ptr == NULL)
+  {
+    perror ("Error. Allocation was unsuccessful"); //displays the string, followed by the textual representation of the current errno value.
+    exit(EXIT_FAILURE);
+  }
     *ptr = value;
     printf("test of allocated memory: %i\n");
 }
